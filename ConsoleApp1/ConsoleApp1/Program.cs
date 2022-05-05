5a6e23aa8371b2c3b965812b56a5e36ade6b914c
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-05 21:27:27 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Какой у Вас знак зодиака?");
+            string zodiak = Console.ReadLine();
+            Console.WriteLine("Чем Вы занимаетесь");
+            string work = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {name}, Вам {age} лет, Ваш знак зодиака {zodiak} и Вы {work}");
+
+        }
+    }
+}
